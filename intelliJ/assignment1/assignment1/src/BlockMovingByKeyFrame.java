5febd6b172cede0e673f3b49a3b549fb75bafe88
AengGyu <cyg0115@naver.com>
2024-11-15 02:13:14 +0900

    addScore

--- intelliJ/assignment1/assignment1/src/BlockMovingByKeyFrame.java
+++ intelliJ/assignment1/assignment1/src/BlockMovingByKeyFrame.java
@@ -1,24 +1,24 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Container;
-import java.awt.GridLayout;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 import java.util.Random;
 
 public class BlockMovingByKeyFrame extends JFrame {
     public BlockMovingByKeyFrame() {
         super("상하좌우 키로 블록 이동시키기_최영규"); // 제목과 제출자 이름 쓰기
-        setSize(300, 300);
+        setSize(500, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         Container c = getContentPane();
-        c.add(new JLabel("상하좌우 키로 블록을 이동시킬 수 있습니다."), BorderLayout.NORTH);
-        MyPanel gridPanel = new MyPanel();
+        JPanel northPanel = new JPanel(); // 설명과 점수를 표시할 패널 생성
+        northPanel.setLayout(new FlowLayout(FlowLayout.LEFT)); // 패널의 레이아웃을 왼쪽 정렬로 설정
+        northPanel.add(new JLabel("상하좌우 키로 블록을 이동시킬 수 있습니다.")); // 패널에 라벨 붙히기
+        JLabel scoreLabel = new JLabel("점수 : 0"); // 점수를 나타낼 라벨을 생성
+        northPanel.add(scoreLabel); // 패널에 붙히기
+        c.add(northPanel,BorderLayout.NORTH); // 패널을 NORTH에 붙히기
+        MyPanel gridPanel = new MyPanel(scoreLabel);
         c.add(gridPanel, BorderLayout.CENTER);
+
         setVisible(true);
         gridPanel.setFocusable(true);
         gridPanel.requestFocus();
@@ -27,14 +27,14 @@ public class BlockMovingByKeyFrame extends JFrame {
     static class MyPanel extends JPanel {
         private final static int ROWS = 5, COLS = 5; // 5x5 그리드
         private JLabel labels[][] = new JLabel[ROWS][COLS]; // 각 그리드 셀에 배치된 JLabel 컴포넌트
-        private int curCol = 2, curRow = 2; // 현재 선택된 블록의 위치
+        private int curCol = 0, curRow = 0; // 현재 선택된 블록의 위치
         private int starCol, starRow; // 별의 위치
         private final static Color BLOCKCOLOR = Color.BLUE; // 선택된 블록의 배경색
         private final static Color LINECOLOR = Color.PINK; // 그리드 선을 표현할 색, 이 패널의 배경색
         private final static Color BASECOLOR = Color.WHITE; // 선택 안 된 블록들의 배경색
         private final static Color STARCOLOR = Color.YELLOW; // 목표 위치 배경색
-
-        public MyPanel() {
+        private int score = 0;
+        public MyPanel(JLabel scoreLabel) {
             setLayout(new GridLayout(ROWS, COLS, 1, 1)); // 상하 좌우 셀의 간격 1픽셀
             setBackground(LINECOLOR); // 배경색을 입히고, 블록을 출력되면 배경색이 1픽셀만 보이게 되어, 그리드 선처럼 보이게 됨
             for (int i = 0; i < labels.length; i++) {
@@ -83,6 +83,8 @@ public class BlockMovingByKeyFrame extends JFrame {
                     labels[curRow][curCol].setBackground(BLOCKCOLOR);
                     if(curRow == starRow && curCol == starCol){ // 파란색 블록과 별이 만나면 별의 위치 재설정
                         setStarPoint();
+                        score++;
+                        scoreLabel.setText("점수 : " + score);
                     }
                     ((Component) e.getSource()).repaint(); // 블록의 배경색에 변화가 발생하였기 때문에 다시 그리기 호출
                 }
@@ -101,7 +103,6 @@ public class BlockMovingByKeyFrame extends JFrame {
             return COLS * y + x;
         }
     }
-
 
     public static void main(String[] args) {
         new BlockMovingByKeyFrame();
